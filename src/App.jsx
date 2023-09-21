638acd85655fbc60b7f6e364318edb9ed519fbbf
Jonathan Mantello <38162257+JMantello@users.noreply.github.com>
2023-09-21 11:43:37 -0700

    reset app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import { useState } from 'react'
-import './App.css'
-import Nav from './components/Nav'
-import Articles from './sections/Articles'
-import Hero from './sections/Hero'
 
 function App() {
 
   return (
-    <main className='app'>
-      <Nav />
-      <Hero />
-      <Articles />
-    </main>
+    <>
+
+    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { NewsProvider } from './contexts/NewsContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <NewsProvider>
-      <App />
-    </NewsProvider>
+    <App />
   </React.StrictMode>,
 )
