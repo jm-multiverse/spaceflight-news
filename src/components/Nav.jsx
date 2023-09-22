23bf9a6488a5ce451e7ff4ec04f85bbac807b38b
Jonathan Mantello <38162257+JMantello@users.noreply.github.com>
2023-09-22 12:46:05 -0700

    add feedback to nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,16 +8,16 @@ const Nav = () => {
       <nav className='flex justify-between items-center max-container'>
         <a className='flex justify-between items-center gap-4' href="/">
           <img
-            className="w-12 h-12 sm:w-16 sm:h-16"
+            className="w-12 h-12 sm:w-16 sm:h-16 hover:animate-spin transition-all"
             src={blackSatelliteOutline}
             alt="Logo"
           />
-          <h1 className='sm:text-xl'>Spaceflight News</h1>
+          <h1 className='sm:text-xl hover:font-medium'>Spaceflight News</h1>
         </a>
         <ul className="flex-1 flex justify-end items-center gap-8 me-8 max-lg:hidden">
           {navLinks.map((item) => (
             <li key={item.label}>
-              <a href={item.href} className='leading-normal text-lg text-slate-gray'>
+              <a href={item.href} className='leading-normal text-lg text-slate-gray hover:font-medium'>
                 {item.label}
               </a>
             </li>
