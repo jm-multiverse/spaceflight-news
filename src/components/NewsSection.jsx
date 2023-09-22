829cfcc4d2c9ca750e1550ecf3026b8b84349d27
Jonathan Mantello <38162257+JMantello@users.noreply.github.com>
2023-09-22 12:38:55 -0700

    update pills

--- src/components/NewsSection.jsx
+++ src/components/NewsSection.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
 export default function NewsSection({ title, newsResponse }) {
-
+  console.log(newsResponse)
   return (
     <section className='bg-inherit rounded'>
-      <a className='flex justify-start items-baseline gap-6 p-3 mt-10 mb-6 group' href="/">
+      <a className='flex justify-start items-baseline gap-6 p-3 pb-5 mt-10 mb-4 group' href="/">
         <h2 className='text-2xl md:text-3xl font-semibold'>
           {title}
         </h2>
@@ -16,14 +16,14 @@ export default function NewsSection({ title, newsResponse }) {
             const date = new Date(article.published_at)
             return (
               <article key={article.id}
-                className='p-8 mb-8 bg-white border-white border-2 rounded-lg shadow-lg hover:cursor-pointer hover:translate-x-1 hover:border-slate-800 transition ease-linear'
+                className='p-8 mb-8 bg-white border-white border-2 rounded-lg shadow-lg hover:cursor-pointer hover:translate-x-2 hover:border-slate-800 transition ease-linear'
               >
                 <a href={article.url} target='_blank'>
                   <h2 className='md:text-2xl font-medium mb-4'>{article.title}</h2>
                   <p className='md:text-lg font-normal mb-4'>{article.summary}</p>
-                  <div className='flex flex-col gap-1 sm:flex-row sm:gap-4'>
-                    <p className='text-sm font-normal text-slate-600 border p-1 px-2 rounded-2xl'>{date.toLocaleDateString()}</p>
-                    <p className='text-sm font-normal text-slate-600 border p-1 px-2 rounded-2xl'>{article.news_site}</p>
+                  <div className='flex flex-col gap-2 sm:flex-row sm:gap-4'>
+                    <p className='text-sm font-normal text-slate-600 border p-1 px-2 rounded-2xl w-fit'>{date.toLocaleDateString()}</p>
+                    <p className='text-sm font-normal text-slate-600 border p-1 px-2 rounded-2xl w-fit'>{article.news_site}</p>
                   </div>
                 </a>
               </article>
