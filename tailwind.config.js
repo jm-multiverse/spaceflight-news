9917ba3b8359487c056799e70240e65c5abb6c66
Jonathan Mantello <38162257+JMantello@users.noreply.github.com>
2023-10-08 12:34:27 -0700

    config tailwind globals

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,18 @@ export default {
       },
       backgroundImage: {
         'constellation': 'url("/src/assets/backgrounds/constellation-light-3.svg")',
+        // make light and dark backgrounds
+      },
+      colors: {
+        primary: "#00040f",
+        secondary: "#00f6ff",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        dimBlue: "rgba(9, 151, 124, 0.1)",
       },
       display: ["group-hover"],
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
     },
     screens: {
       xs: "480px",
