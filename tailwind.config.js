1595ad70bd71f6ea525f44c192406bc86cc482d6
Jonathan Mantello <38162257+JMantello@users.noreply.github.com>
2023-10-08 12:32:23 -0700

    Add screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,14 @@ export default {
       },
       display: ["group-hover"],
     },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
   },
   plugins: [],
 }
