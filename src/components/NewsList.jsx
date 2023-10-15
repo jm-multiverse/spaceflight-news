60c165dc42093a5a1e0c8a0c118ced8a2a96e382
Jonathan Mantello <38162257+JMantello@users.noreply.github.com>
2023-10-15 15:46:40 -0700

    increase news title font on small screens

--- src/components/NewsList.jsx
+++ src/components/NewsList.jsx
@@ -11,7 +11,7 @@ export default function NewsList({ newsResponse }) {
               className='p-8 mb-8 bg-white border-white border-2 rounded-lg shadow-lg hover:cursor-pointer hover:translate-x-2 hover:border-slate-800 transition ease-linear'
             >
               <a href={article.url} target='_blank'>
-                <h2 className='md:text-2xl font-medium mb-4'>{article.title}</h2>
+                <h2 className='md:text-2xl text-lg font-medium mb-4'>{article.title}</h2>
                 <p className='md:text-lg font-normal mb-4'>{article.summary}</p>
                 <div className='flex flex-col gap-2 sm:flex-row sm:gap-4'>
                   <p className='text-sm font-normal text-slate-600 border p-1 px-2 rounded-2xl w-fit'>{date.toLocaleDateString()}</p>
